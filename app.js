34b89de8af1743800df3113b341414af83278581
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-31 22:53:09 +0200

    render blogs on HP

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 
 const Blog = require('./models/blog')
+const { find } = require('lodash')
 
 const app = express()
 
@@ -22,61 +23,29 @@ app.use(express.static('public'))
 app.use(morgan('dev'))
 
 //mongo
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'New blog 2',
-        snippet: 'Aboy new blog',
-        body: 'some more about my new blog fro you here to read on'
-    }) 
 
-    blog.save()
-        .then(result => {
-            res.send(result)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
 
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then(result => res.send(result))
-        .catch(err => console.log(err))
-})
 
-app.get('/single-blog', (req, res) => {
-    Blog.findById('6477afb6772af2d40fee4dec')
-        .then(result => res.send(result))
-        .catch(err => console.log(err))
-})
-
-app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next()
-})
-
-app.use((req, res, next) => {
-    console.log('in the next middleware');
-   
-    next()
-})
-
+// basic routes
 app.get('/', (req, res) => {
-
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-
-    res.render('index', {title: 'Home', blogs})
+    res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
+})
+
+
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1})
+        .then(result => {
+            res.render('index', {
+                title: 'All Blogs',
+                blogs: result 
+            })
+        })
+        .catch(err => console.log(err))
 })
 
 app.get('/blogs/create', (req, res) => {
