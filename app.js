88ea6b5174cf1960cc940e243e30d4b7c12af764
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-31 21:27:05 +0200

    MDB + env

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
+require('dotenv').config()
+
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const Blog = require('./models/blog')
+
 const app = express()
+
+// connect to Mongo
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(res => app.listen(3000))
+    .catch(err => console.log(err))
 
 // /registe view engine
 app.set('view engine', 'ejs');
 
-// listen
-app.listen(3000);
+
+//middleware statis files
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
+//mongo
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New blog 345',
+        snippet: 'Aboy new blog',
+        body: 'some more about my new blog fro you here to read on'
+    }) 
+
+    blog.save()
+        .then(result => {
+            res.send(result)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('in the next middleware');
+   
+    next()
+})
 
 app.get('/', (req, res) => {
 
